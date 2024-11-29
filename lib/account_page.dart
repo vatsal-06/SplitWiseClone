6d04e975fe31199094ec3790b54feaf2f4d5e805
Vatsal Gupta <tsrvatsal17@gmail.com>
2024-11-29 09:11:37 +0530

    Account Page Edit

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -26,162 +26,232 @@ class _AccountPageState extends State<AccountPage> {
         ),
       ),
 
-      body: Container(
-        margin: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Account',
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-              ),
-            ),
-            const SizedBox(height: 25),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Settings',
-              ),
-            ),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AccountSettings()));
-                });
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    const Icon(
-                      Icons.person,
-                      size: 40,
-                    ),
-                    const SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Vatsal Gupta', style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        Text('tsrvatsal17@gmail.com', style: Theme.of(context).textTheme.bodySmall,),
-                      ],
-                    ),
-                    const Spacer(),
-                    const Icon(Icons.arrow_forward_ios, size: 20,)
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Account',
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
                 ),
               ),
-            ),
-
-            const Divider(),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
+              const SizedBox(height: 25),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Settings',
+                ),
+              ),
+              const SizedBox(height: 10),
+              GestureDetector(
                 onTap: () {
                   setState(() {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const TestPage()));
+                            builder: (context) => const AccountSettings()));
                   });
                 },
-                child: Row(
-                  children: [
-                    const Icon(Icons.qr_code_2_sharp, size: 40,),
-                    const SizedBox(width: 10), 
-                    Text('Scan Code', style: Theme.of(context).textTheme.bodyLarge,), 
-                    const Spacer(), 
-                    const Icon(Icons.arrow_forward_ios, size: 20,)
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.person,
+                        size: 40,
+                      ),
+                      const SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Vatsal Gupta', style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          Text('tsrvatsal17@gmail.com', style: Theme.of(context).textTheme.bodySmall,),
+                        ],
+                      ),
+                      const Spacer(),
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
                     ],
-                    ),
+                  ),
+                ),
               ),
-            ),
-
-            const Divider(),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const TestPage()));
-                  });
-                },
-                child: Row(
-                  children: [
-                    const Icon(Icons.diamond_sharp, size: 40, color: Colors.deepPurple,),
-                    const SizedBox(width: 10), 
-                    Text('Splitwise Pro', style: Theme.of(context).textTheme.bodyLarge,), 
-                    const Spacer(), 
-                    const Icon(Icons.arrow_forward_ios, size: 20,)
-                    ],
-                    ),
+        
+              const Divider(),
+        
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      const Icon(Icons.qr_code_2_sharp, size: 40,),
+                      const SizedBox(width: 10), 
+                      Text('Scan Code', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
               ),
-            ),
-
-            const SizedBox(height: 25),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text('Preferences'),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const TestPage()));
-                  });
-                },
-                child: Row(
-                  children: [
-                    Text('Notifications', style: Theme.of(context).textTheme.bodyLarge,), 
-                    const Spacer(), 
-                    const Icon(Icons.arrow_forward_ios, size: 20,)
-                    ],
-                    ),
+        
+              const Divider(),
+        
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      const Icon(Icons.diamond_sharp, size: 40, color: Colors.deepPurple,),
+                      const SizedBox(width: 10), 
+                      Text('Splitwise Pro', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
               ),
-            ),
-
-            const Divider(),
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const TestPage()));
-                  });
-                },
-                child: Row(
-                  children: [
-                    Text('Security', style: Theme.of(context).textTheme.bodyLarge,), 
-                    const Spacer(), 
-                    const Icon(Icons.arrow_forward_ios, size: 20,)
-                    ],
-                    ),
+        
+              const SizedBox(height: 25),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Preferences'),
               ),
-            ),
+        
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Text('Notifications', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
+              ),
+        
+              const Divider(),
+        
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Text('Security', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
+              ),
+        
+              const SizedBox(height: 25),
+        
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Feedback'),
+              ),
+        
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Text('Rate Splitwise', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
+              ),
 
-            const SizedBox(height: 25),
-          ],
+              const Divider(),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Text('Contact Us', style: Theme.of(context).textTheme.bodyLarge,), 
+                      const Spacer(), 
+                      const Icon(Icons.arrow_forward_ios, size: 20,)
+                      ],
+                      ),
+                ),
+              ),
+
+              const Divider(),
+
+              Center(
+                child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TestPage()));
+                    });
+                  },
+                  child: const Text('Log out', style: TextStyle(fontSize: 12, color: Colors.green),),
+                ),
+              ),
+              )
+            ],
+          ),
         ),
       ),
     );
